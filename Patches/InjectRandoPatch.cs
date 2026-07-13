d8a7fd8cb354b4ceff18eef344eedcb2fd11e198
RobertSPratley <rspratley1126@gmail.com>
2026-07-12 20:02:42 -0400

    can detect all checks except shops

--- Patches/InjectRandoPatch.cs
+++ Patches/InjectRandoPatch.cs
@@ -6,24 +6,32 @@ using MelonLoader;
 using UnityEngine;
 using Il2CppRotaryHeart.Lib.SerializableDictionary;
 
-namespace DemonTidesAP.Patches.Tests;
+namespace DemonTidesAP.Patches;
 
 [HarmonyPatch(typeof(TitleMenu), "StartGame")]
 public static class InjectRandoPatch
 {
-    private static bool injectiontest = true;
+    public static string[] slot_IDs = ["c07ae6d0-7fa5-4486-a987-f0877945c697", "3a38dd60-83e9-4810-b4a4-a33a11df3d6f", "f8d84de9-b4fd-4946-ac04-d2f518a8e789"];
 
     static void Postfix(ref TitleMenu __instance)
     {
         SaveDataManager manager = GameObject.Find("Save Data Manager").GetComponent<SaveDataManager>();
         SaveData savedata = manager.CurrentSaveData;
-        if (savedata.randomizerActive && Core.Debug && injectiontest)
+        if (savedata.randomizerActive && Core.Debug)
         {
             foreach(string key in savedata.randomizerDictionary._keys)
             {
-                savedata.randomizerDictionary[key] = "0d90281d-ff36-4c50-8fb5-40c672da5916";
+                savedata.randomizerDictionary[key] = Core.DisplayItemID;
             }
-            //savedata.randomizerLuciUnlocks["Lokitana"].list[0] = "1";
+
+            foreach(string key in slot_IDs)
+            {
+                if (!savedata.randomizerDictionary.ContainsKey(key))
+                {
+                    savedata.randomizerDictionary.Add(key, Core.DisplayItemID);
+                }
+            }
+
         }
     }
     
