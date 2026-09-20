1211fa33f9b4e235dc3affcc449d579278f8fa8f
RobertSPratley <rspratley1126@gmail.com>
2026-09-20 19:11:51 -0400

    New Features: - Vindra can now be detected - Islands can now be locked

---
+++ Patches/CheckDetection/VindraPatch.cs
@@ -0,0 +1,21 @@
+﻿using Archipelago.MultiClient.Net.Models;
+using DemonTidesAP.Helpers;
+using HarmonyLib;
+using Il2CppFabraz;
+using Il2CppFabraz.SaveData;
+using Il2CppFabraz.UI;
+using Il2CppRotaryHeart.Lib.SerializableDictionary;
+using MelonLoader;
+using UnityEngine;
+
+namespace DemonTidesAP.Patches.CheckDetection;
+
+[HarmonyPatch(typeof(PostVindraSceneWrapup), "Trigger")]
+public static class VindraPatch
+{
+    static void Postfix(PostVindraSceneWrapup __instance)
+    {
+        string check_name = "Vindra";
+        Core.LocationDetected(check_name);
+    }
+}
